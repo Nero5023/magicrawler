1ea51aec398ead20f32c06741db0030efe6f1944
Nero Zuo <502350123@qq.com>
2016-10-25 22:42:28 +0800

    First commit

---
+++ Sources/magicrawler.swift
@@ -0,0 +1,4 @@
+struct magicrawler {
+
+    var text = "Hello, World!"
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import magicrawlerTests
+
+XCTMain([
+     testCase(magicrawlerTests.allTests),
+])
